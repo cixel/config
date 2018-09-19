8c84002d71ea01a1f212c5195f9eda5e02278c3b
ehdens <ehdens@Ehdens-MacBook-Pro.local>
2018-09-19 16:55:54 -0400

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-cp ~/.zshrc ~/.zshrc.bak && rm ~/.zshrc && ln -s ./zshrc ~/.zshrc
+if [ -f /Users/ehdens/.zshrc ]; then
+	echo "backup up ~/.zshrc"
+	mv /Users/ehdens/.zshrc /Users/ehdens/.zshrc.bak
+fi
+
+ln -s .zshrc /Users/ehdens/.config/.zshrc
