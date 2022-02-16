d0621ee4e7200306340785061dace6196261ef15
Ehden Sinai <ehdens@gmail.com>
2022-02-16 11:35:01 -0500

    nvim-lint: remove unused variable

--- nvim/config/plugins/nvim-lint.lua
+++ nvim/config/plugins/nvim-lint.lua
@@ -1,15 +1,9 @@
 local lint = require('lint')
 
-local severities = {
-	error = vim.lsp.protocol.DiagnosticSeverity.Error,
-	warning = vim.lsp.protocol.DiagnosticSeverity.Warning,
-	refactor = vim.lsp.protocol.DiagnosticSeverity.Information,
-	convention = vim.lsp.protocol.DiagnosticSeverity.Hint,
-}
-
 lint.linters_by_ft = {
 	go = {'golangcilint'},
 }
+
 lint.linters.golangcilint.args = {
 	'run',
 	'--out-format', 'json',
