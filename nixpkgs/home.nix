cf4969216525a2ca523280188ed85aa1149a40b9
Ehden Sinai <ehdens@gmail.com>
2022-03-29 09:52:12 -0400

    go: add patch to revert fsync workaround for darwin

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -33,6 +33,26 @@ in
   #   }))
   # ];
 
+  # fsync on darwin is broken in that it's not a true fsync, so go's
+  # syscall wrapper calls F_FULLFSYNC instead to make sure that writes are
+  # actually flushed as expected. In general, with a laptop, there's probably
+  # no issue, because the battery controler should handle flushing cache when
+  # there's a sudden power loss or something. Down the rabbit hole of tweets is
+  # more justification on this. This patch changes it to just call fsync. I
+  # never use darwin besides on a laptop, so it should be safe enough for me.
+  #
+  # https://github.com/golang/go/issues/28739
+  nixpkgs.overlays = [
+    (self: super: {
+      go = super.go.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ (
+          if lib.stdenv.isDarwin then [ ./fd_fsync_darwin.patch ]
+          else [ ]
+        );
+      });
+    })
+  ];
+
   # services.lorri.enable = true;
   home.packages = with pkgs; [
     direnv
@@ -91,10 +111,10 @@ in
     pre-commit
   ];
 
-  programs.neovim   = import ./nvim-conf.nix { pkgs = pkgs; };
-  programs.starship = import ./starship-conf.nix { pkgs = pkgs; lib = lib; };
-  programs.tmux     = import ./tmux-conf.nix { pkgs = pkgs; };
-  programs.zsh      = import ./zsh-conf.nix { pkgs = pkgs; };
+  programs.neovim = import ./nvim-conf.nix { inherit pkgs; };
+  programs.starship = import ./starship-conf.nix { inherit pkgs lib; };
+  programs.tmux = import ./tmux-conf.nix { inherit pkgs; };
+  programs.zsh = import ./zsh-conf.nix { inherit pkgs; };
 
   programs.fzf = {
     enable = true;
@@ -109,7 +129,7 @@ in
 
   programs.go = {
     enable = true;
-    package = pkgs.go_1_17;
+    package = pkgs.go;
     goPath = "${builtins.getEnv "HOME"}/gopath";
     goBin = "${builtins.getEnv "HOME"}/gobin";
   };
