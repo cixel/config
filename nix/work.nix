70a9e430ec5b2945c7bc36b69b9eb5a4e91dda51
Ehden Sinai <ehden@contrastsecurity.com>
2024-10-11 14:58:38 -0400

    nix: update /etc/resolvers on work machines

--- nix/work.nix
+++ nix/work.nix
@@ -1,27 +1,21 @@
 # work-machine specific stuff - like env vars needed for netskope
 { pkgs, ... }:
 {
-  environment.variables = let
-    # this is all public keys, but IT doesn't want us throwing the bundle in
-    # public repos so this is just the path my work machines would both expect
-    # to find it.
-    path = "/Library/Application\ Support/Netskope/STAgent/data/netskope-cert-bundle.pem";
-  in {
-    NIX_SSL_CERT_FILE = path;
-    CURL_CA_BUNDLE = path;
-    GIT_SSL_CAPATH = path;
-    GIT_SSL_CAINFO = path;
-    SSL_CERT_FILE = path;
-    NODE_EXTRA_CA_CERTS = path;
-  };
-
-  nixpkgs.overlays = [
-    (self: super: {
-      zig = super.zig.overrideAttrs (old: {
-        patches = [ ../home-manager/zig_cert.patch ];
-      });
-    })
-  ];
+  environment.variables =
+    let
+      # this is all public keys, but IT doesn't want us throwing the bundle in
+      # public repos so this is just the path my work machines would both expect
+      # to find it.
+      path = "/Library/Application\ Support/Netskope/STAgent/data/netskope-cert-bundle.pem";
+    in
+    {
+      NIX_SSL_CERT_FILE = path;
+      CURL_CA_BUNDLE = path;
+      GIT_SSL_CAPATH = path;
+      GIT_SSL_CAINFO = path;
+      SSL_CERT_FILE = path;
+      NODE_EXTRA_CA_CERTS = path;
+    };
 
   services.tailscale = {
     enable = true;
@@ -38,4 +32,10 @@
       '';
     };
   };
+
+  # https://github.com/LnL7/nix-darwin/blob/master/modules/services/tailscale.nix#L66C2-L72C7
+  environment.etc."resolver/c.headscale.ehden.net".text = "nameserver 100.100.100.100";
+  environment.etc."resolver/c.headscale.ehden.net".knownSha256Hashes = [
+    "2c28f4fe3b4a958cd86b120e7eb799eee6976daa35b228c885f0630c55ef626c"
+  ];
 }
