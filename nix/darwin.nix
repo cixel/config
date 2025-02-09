d217883de45727acd0aef7c44f3f93b4e8e2dc6c
Ehden Sinai <ehden@contrastsecurity.com>
2025-02-09 00:24:17 -0500

    nix: update flake
    the nix-darwin update removes deprecates services.nix-daemon.enable and
    nix.useDaemon, which have been replaced with nix.enable

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -1,13 +1,8 @@
 { user }: { pkgs, ... }: {
-  # Auto upgrade nix package and the daemon service.
-  # TODO: is it wrong/incorrect to let nix-darwin manage this given that it was
-  # installed by the detsys nix installer?
-  services.nix-daemon.enable = true;
-
   nix = {
     # We install Nix using a separate installer so we don't want nix-darwin
     # to manage it for us. This tells nix-darwin to just use whatever is running.
-    useDaemon = true;
+    enable = true;
   };
   nixpkgs.config = {
     allowUnfree = true;
