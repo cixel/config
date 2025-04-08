443136d8f31ef17ff73a0ce116e71f906dd6538b
Ehden Sinai <ehdens@gmail.com>
2025-04-08 10:03:33 -0400

    jj: add dependabot and pr aliases

--- nix/home-manager/jujutsu.nix
+++ nix/home-manager/jujutsu.nix
@@ -16,9 +16,21 @@
       git_push_bookmark = ''"ehden/push-" ++ change_id.short()'';
     };
 
+    template-aliases = {
+      "bookmarks_at_remote(remote)" = ''
+        remote_bookmarks.filter(|b| b.remote() == remote)
+      '';
+    };
+
     revset-aliases = {
       "closest_bookmark(to)" = "heads(::to & bookmarks())";
       "closest_pushable(to)" = ''heads(::to & ~description(exact:"") & (~empty() | merges()))'';
+      "dependabot_prs" = ''remote_bookmarks(glob:"dependabot/*")'';
+
+      # https://youtu.be/ZnTNFIMjDwg?t=779
+      "stack(x, n)" = "ancestors(reachable(x, mutable()), n)";
+      "stack(x)" = "stack(x, 2)";
+      "stack()" = "stack(@)";
     };
 
     aliases = {
@@ -33,6 +45,28 @@
       rb = [ "rebase" "-d" "trunk()" ];
       s = [ "status" "--no-pager" ];
       nt = [ "new" "trunk()" ];
+
+      dependabot_update = [
+        "util" "exec" "--" "bash" "-c" ''
+          jj git fetch
+          jj bookmark track $(jj log -r 'dependabot_prs' -T 'bookmarks_at_remote("origin") ++ " "' --no-graph)
+          jj rebase -d 'trunk()' -r 'dependabot_prs'
+        ''
+      ];
+      dependabot = [
+        "util" "exec" "--" "bash" "-c" ''
+          jj dependabot_update
+          jj git push -r 'trunk()+:: & bookmarks(glob:"dependabot/*")'
+        ''
+      ];
+
+      pr = [
+        "util" "exec" "--" "bash" "-c" ''
+          gh pr create --head $(
+            jj log -r 'closest_bookmark(@)' -T 'bookmarks' --no-graph | cut -d ' ' -f 1
+          )
+        ''
+      ];
     };
   };
 }
