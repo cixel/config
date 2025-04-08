f0f5635345ec1fa6f8fa537015a43814a39b6f53
Ehden Sinai <ehdens@gmail.com>
2025-04-08 10:03:33 -0400

    jj: tweak behavior of ub alias

--- nix/home-manager/jujutsu.nix
+++ nix/home-manager/jujutsu.nix
@@ -16,15 +16,19 @@
       push-bookmark-prefix = "ehden/push-";
     };
 
+    revset-aliases = {
+      "closest_bookmark(to)" = "heads(::to & bookmarks())";
+      "closest_pushable(to)" = ''heads(::to & ~description(exact:"") & (~empty() | merges()))'';
+    };
+
     aliases = {
       l = [ "log" "-r" "(trunk()..@):: | (trunk()..@)-" "--no-pager" ];
       _l = [ "log" "-r" "(trunk()..@):: | (trunk()..@)-" "-T" "builtin_log_detailed" "--no-pager" ];
       lg = [ "log" "-r" "trunk()..@ | ::trunk()" ];
       _lg = [ "log" "-r" "trunk()..@ | ::trunk()" "-T" "builtin_log_detailed" ];
 
-      # NOTE: this is kinda unsafe because it can pull e.g main or next up if
-      # i'm already caught up
-      ub = [ "bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to=@" ];
+      # https://github.com/jj-vcs/jj/discussions/5568#discussioncomment-12674748
+      ub = ["bookmark" "move" "--from" "closest_bookmark(@)" "--to" "closest_pushable(@)"];
 
       rb = [ "rebase" "-d" "trunk()" ];
       s = [ "status" "--no-pager" ];
